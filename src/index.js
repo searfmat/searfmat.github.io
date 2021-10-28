db4af0a88013b612c330b720100c6d677beabaf1
Matthew Searfass <mattsea1028@gmail.com>
2021-10-28 14:18:13 -0400

    Added comments and analytics

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+/* 
+    This script animates the page to scroll to the correct section on menu selection.
 
-let anchorSelector = 'a[href^="#"]';
+    Uses the linkLookup to locate all <a> that mentions a div
+    We then use animate to scroll to the correct part of the page in a smooth manner
 
-$(anchorSelector).on('click', function (e) {
+    Code based on: https://www.geeksforgeeks.org/how-to-set-smooth-scroll-after-clicking-the-link-using-javascript/
+*/
+
+let linkLookup = 'a[href^="#"]';
+
+$(linkLookup).on('click', function (e) {
     e.preventDefault();
-    let destination = $(this.hash);
-    let scrollPosition = destination.offset().top;
-    let animationDuration = 500;
     $('html, body').animate({
-            scrollTop: scrollPosition
-        }, animationDuration);
-    });
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+});
